98891a873b0c41f58a61eb24afcc891f1034bfd0
joaquin <joacoetruuu@gmail.com>
2023-07-03 14:42:56 -0300

    askdj

--- src/scripts/getFechas.js
+++ src/scripts/getFechas.js
@@ -1,10 +1,8 @@
 export async function getFechas(){
-    try {
+   
         const res = await fetch('https://api-fechas-duki-bs4g.vercel.app/');
         const data = await res.json()
         return data; 
 
-      } catch (error) {
-        console.error('Error en la petición Fetch:', error);
-      }
+     
 }; 
